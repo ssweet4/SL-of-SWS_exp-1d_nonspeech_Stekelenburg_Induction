7b2b980ea2e849fdc02c553bcf14be595d4ee63b
Sierra Sweet <ssweet4@uwo.ca>
2022-02-17 14:51:07 -0500

    Fixed typos

--- slTrain.js
+++ slTrain.js
@@ -69,8 +69,8 @@ var sl_instruct = {
 	type: 'html-keyboard-response',
 	stimulus: '<p>In this part of the study, you will hear sounds similar to those you just heard.</p>'+
 			  '<p>Please focus on these sounds, as you may be asked questions about them later.</p>'+
-			  '<p>Occasionally, the sounds will pause for up to two seconds. When this happens, please press SPACEBAR as quickly as you can.</br>This helps ensure that you are paying attention to the sounds.</p>'+
-			  '<p>This part of the study will take about 12 minutes to complete, and you will be given the opportunity to take a couple of short breaks every few minutes.</p>'+
+			  '<p>Occasionally, the sounds will pause for up to four seconds. When this happens, please press SPACEBAR as quickly as you can.</br>This helps ensure that you are paying attention to the sounds.</p>'+
+			  '<p>This part of the study will take about 4 minutes to complete.</p>'+
 			  '<p><strong>Press SPACEBAR to begin</strong></p>',
 	choices: [' ']
 };
